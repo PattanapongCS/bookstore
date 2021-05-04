97930c26e5e27359ee4a1004f0366549373a35d1
MacBook Pro <macbookpro@PU.local>
2021-05-04 18:22:51 +0700

    edit test

--- src/test/java/scb/challenge/book/TestService.java
+++ src/test/java/scb/challenge/book/TestService.java
@@ -4,8 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,13 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
 import scb.challenge.book.model.JwtRequest;
-import scb.challenge.book.model.UserPostRequest;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,7 +35,6 @@ public class TestService {
 	private String url="http://localhost:";
 	
 	private RestTemplate restTemplate = new RestTemplate();
-	
 	
 	
 	@Test
@@ -62,24 +65,31 @@ public class TestService {
 		assertEquals(200, result.getStatusCodeValue());
 	}
 	
-//	@Test
-	@DisplayName("unit test of the create user service")
-	public void createUserSuccess() throws URISyntaxException {
-		final String baseUrl = url + randomServerPort + "/user";
+	@Test
+	@DisplayName("unit test of the get user service")
+	public void getUser() throws URISyntaxException, JSONException {
+		final String baseUrl = url + randomServerPort + "/login";
 		URI uri = new URI(baseUrl);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		UserPostRequest req = new UserPostRequest();
-		req.setName("test name");
-		req.setSurname("test surname");
-		req.setDate_of_birth(new Date());
-		req.setUsername("test");
-		req.setPassword("test");
-		HttpEntity<UserPostRequest> requestEntity = new HttpEntity<>(req, headers);
+		JwtRequest req = new JwtRequest();
+		req.setUsername("admin");
+		req.setPassword("admin");
+		HttpEntity<JwtRequest> requestEntity = new HttpEntity<>(req, headers);
 		ResponseEntity<String> result = restTemplate.postForEntity(uri,requestEntity, String.class);
-
-		// Verify success
-		assertEquals(200, result.getStatusCodeValue());
+		if (StringUtils.isNotBlank(result.getBody())) {
+			JSONObject obj = new JSONObject(result.getBody());
+			String getBaseUrl = url + randomServerPort + "/user?id={id}";
+			HttpHeaders getHeaders = new HttpHeaders();
+			getHeaders.setContentType(MediaType.APPLICATION_JSON);
+			getHeaders.set("Authorization", obj.getString("token"));
+			Map<String, Object> getReq = new HashMap<>();
+			getReq.put("id", 1);
+			HttpEntity<?> httpEntity  = new HttpEntity<>(getHeaders); 
+			ResponseEntity<String> getResult = restTemplate.exchange(getBaseUrl, HttpMethod.GET, httpEntity,String.class, getReq);
+			
+			assertEquals(200, getResult.getStatusCodeValue());
+		}
 	}
 	
 }
